dda80e1ed7cfe4dcea32a0395db10e8774a2b872
wangjiabao <2107633156@qq.com>
2024-07-12 23:55:26 +0800

    feat: local cache BatchGet impl

--- cache/cache.go
+++ cache/cache.go
@@ -10,8 +10,31 @@ import (
 type Cache interface {
 	Set(ctx context.Context, key string, value any, expire time.Duration) error         // 设置缓存
 	BatchSet(ctx context.Context, values map[string][]byte, expire time.Duration) error // 批量设置缓存
-	Get(ctx context.Context, key string) ([]byte, error)                                // 获取缓存
-	BatchGet(ctx context.Context, keys []string) (map[string][]byte, error)             // 批量获取缓存
-	Del(ctx context.Context, key string) error                                          // 删除缓存
-	BatchDel(ctx context.Context, keys []string) error                                  // 批量删除缓存
+
+	//
+	// BatchGet
+	//  @Description: 批量查询缓存
+	//  @param ctx
+	//  @param keys
+	//  @return map[string][]byte	缓存中的值
+	//  @return []string			缓存中没有查询到的值
+	//  @return error				查询过程中出现的异常
+	//
+	BatchGet(ctx context.Context, keys []string) (map[string][]byte, []string, error)
+	//
+	// BatchDel
+	//  @Description: 批量删除缓存
+	//  @param ctx
+	//  @param keys
+	//  @return error
+	//
+	BatchDel(ctx context.Context, keys []string) error
+	//
+	// IsNotFoundErr
+	//  @Description: 判断 err 是否为记录不存在
+	//  @param ctx
+	//  @param err
+	//  @return bool
+	//
+	IsNotFoundErr(ctx context.Context, err error) bool
 }
--- cache/local_cache.go
+++ cache/local_cache.go
@@ -39,19 +39,50 @@ func NewLocalCache[T any](loader Loader, config *Config) *LocalCache[T] {
 //	@return map[string][]byte
 //	@return error
 func (l *LocalCache[T]) BatchGet(ctx context.Context, keys []string) (map[string][]byte, error) {
+
 	result := make(map[string][]byte, len(keys))
+
 	if len(keys) == 0 {
 		return result, nil
 	}
 
-	cacheValueMap, err := l.cache.BatchGet(ctx, keys)
-
+	// batch query from cache
+	cacheValueMap, notFoundKeys, err := l.cache.BatchGet(ctx, keys)
 	if err != nil {
-		// todo 日志打印
-		return nil, err
+		// todo 打印日志，查询出现了异常，需要降级回源
+		sourceValueMap, loaderErr := l.loader(ctx, keys)
+		if loaderErr != nil {
+			// todo 打印日志，回源也出现了异常
+			return nil, err
+		}
+
+		// return source value
+		return sourceValueMap, nil
 	}
 
-	return cacheValueMap, nil
+	// add cache value to result
+	for key, val := range cacheValueMap {
+		result[key] = val
+	}
+
+	// handle not found keys
+	if len(notFoundKeys) > 0 {
+		// loader source value
+		sourceValueMap, loaderErr := l.loader(ctx, notFoundKeys)
+		if loaderErr != nil {
+			// todo 打印日志，回源也出现了异常
+			return nil, err
+		}
+
+		// add source value to result
+		for key, val := range sourceValueMap {
+			result[key] = val
+		}
+	}
+
+	// todo 缓存穿透情况处理
+
+	return result, nil
 }
 
 // BatchDel
